51345923fc1a43312554c436e82bca31b0f0bb8a
Plidher <65611683+PLNSTD@users.noreply.github.com>
2024-10-12 18:18:13 -0400

    Added new optimized solution

--- 6 Kyu/SplitStrings.py
+++ 6 Kyu/SplitStrings.py
@@ -4,6 +4,14 @@ If the string contains an odd number of characters then it should replace the mi
 '''
 def solution(s):
     output = []
+    if len(s) % 2:
+        s += '_'
+    for i in range(0, len(s), 2):
+        output.append(s[i:i+2])
+    return output
+
+def solution(s):
+    output = []
     new_pair = ''
     for i in range(len(s)):
         if i == 0 or i % 2 == 0:
