1f72d77e371ec604fb58da4103694ccbcd6ed4ec
Plidher <65611683+PLNSTD@users.noreply.github.com>
2024-12-08 18:08:25 -0500

    Added Kata 6/ UniqueInOrder

---
+++ 6 Kyu/UniqueInOrder.py
@@ -0,0 +1,22 @@
+'''
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
+unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3]
+'''
+
+def unique_in_order(sequence):
+    unique_seq = []
+    last_char = None
+    
+    for char in sequence:
+        if char == last_char:
+            continue
+        last_char = char
+        unique_seq.append(char)
+    
+    return unique_seq
